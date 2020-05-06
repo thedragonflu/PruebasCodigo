abafe4cb80e6d3b1631a325e57ed3a4de395c196
Erick Chasco <pantuflin4@gmail.com>
2020-05-06 17:27:35 +0200

    Modelo cliente

---
+++ Pages/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebasCodigo.Pages.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Contraseña { get; set; }
+    }
+}
