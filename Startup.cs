44042ac46177e8efeb36deb57cc1fe11bfffe7d6
Erick Chasco <pantuflin4@gmail.com>
2020-05-06 17:37:47 +0200

    Editar Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PruebasCodigo.Models;
 
 namespace PruebasCodigo
 {
@@ -31,7 +33,7 @@ namespace PruebasCodigo
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
