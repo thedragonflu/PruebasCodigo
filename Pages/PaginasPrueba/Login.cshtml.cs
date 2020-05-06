5b206d18fbdebcdd40b5ca1e7a92d16382f6ec59
Erick Chasco <pantuflin4@gmail.com>
2020-05-06 18:55:10 +0200

    Primera edicion login

--- Pages/PaginasPrueba/Login.cshtml.cs
+++ Pages/PaginasPrueba/Login.cshtml.cs
@@ -4,14 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PruebasCodigo.Models;
 
 namespace PruebasCodigo.Pages.PaginasPrueba
 {
     public class LoginModel : PageModel
     {
+
+        private readonly Context _db;
+
+        public LoginModel(Context db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Cliente Cliente { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Add(Cliente);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
